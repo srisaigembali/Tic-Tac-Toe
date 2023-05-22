81702601738628bd32f6ef7b1225c9eb6f6b4109
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2023-05-22 13:26:56 +0530

    ttt game

---
+++ script.js
@@ -0,0 +1,67 @@
+console.log("Welcome to Tic Tac Toe")
+let music = new Audio("music.mp3");
+let turnmusic = new Audio("ting.mp3");
+let gameovermusic = new Audio("gameover.mp3");
+let turn = "X";
+let isgameover = false;
+
+// function to change the turn
+const changeTurn = () => {
+    return turn == "X" ? "O" : "X";
+}
+
+// function to check for a win
+const checkWin = () => {
+    let boxtexts = document.getElementsByClassName("boxtext");
+    let wins = [
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
+    ];
+    wins.forEach(e => {
+        if((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[1]].innerText === boxtexts[e[2]].innerText) && (boxtexts[e[0]].innerText !== "")) {
+            document.querySelector('.info').innerText = boxtexts[e[0]].innerText + " Won";
+            gameovermusic.play();
+            isgameover = true;
+            document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "200px";
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
+            document.querySelector(".line").style.width = "20vw";
+        }
+    })
+}
+
+// game logic
+// music.play();
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(element => {
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', (e) => {
+        if(boxtext.innerText === '') {
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            turnmusic.play();
+            checkWin();
+            if(!isgameover) 
+                document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+        }
+    })
+})
+
+// add onclick listener to reset button
+const resetfunction = () => {
+    let boxtexts = document.getElementsByClassName("boxtext");
+    Array.from(boxtexts).forEach(e => {
+        e.innerText = "";
+    });
+    turn = "X";
+    isgameover = false;
+    document.querySelector(".line").style.width = "0vw";
+    document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "0";
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+}
+reset.addEventListener('click', resetfunction);
